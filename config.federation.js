7d32773dfc2a395bb2064e027c7c18a24d9e9229
andre-violin <andre.violin@ifms.edu.br>
2025-04-17 17:46:07 -0400

    initial settings for the host application

---
+++ config.federation.js
@@ -0,0 +1,19 @@
+export default {
+  name: 'app',
+  remotes: {
+    // remote_app1: 'https://localhost:5005/assets/remoteEntry.js'
+  },
+  shared: [
+    'vue',
+    'vuetify',
+    '@apollo/client',
+    '@vue/apollo-composable',
+    'axios',
+    'core-js',
+    'graphql',
+    'graphql-tag',
+    'pinia',
+    'vue-router',
+    'qrcode-vue3'
+  ]
+}
---
+++ src/pages/dashboard/RemoteApp1.vue
@@ -0,0 +1,10 @@
+<template>
+  <ComponenteApp1 v-if="!!ComponenteApp1" />
+</template>
+
+<script setup>
+import { defineAsyncComponent } from 'vue'
+const ComponenteApp1 = defineAsyncComponent(
+  () => import('remote_app1/ComponenteApp1')
+)
+</script>
